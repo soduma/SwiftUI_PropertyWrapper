7f6b0e3a610749eae4afcfaf9f484443916408e7
soduma <soduma@naver.com>
2022-05-03 11:27:09 +0900

    update

--- SwiftUI_PropertyWrapper/ContentView.swift
+++ SwiftUI_PropertyWrapper/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+final class ThingStore: ObservableObject {
+  @Published var things: [String] = []
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var showAddThing = false
+  @StateObject private var myThings = ThingStore()
+
+  var body: some View {
+    NavigationView {
+      VStack(spacing: 20) {
+        if myThings.things.isEmpty {
+          Text("Add acronyms you learn")
+            .foregroundColor(.gray)
+        }
+        ForEach(myThings.things, id: \.self) { thing in
+          Text(thing)
+        }
+        Spacer()
+      }
+      .sheet(isPresented: $showAddThing) {
+        AddThingView(someThings: myThings)
+      }
+      .navigationTitle("TIL")
+      .toolbar {
+        ToolbarItem {
+          // swiftlint:disable:next multiple_closures_with_trailing_closure
+          Button(action: { showAddThing.toggle() }) {
+            Image(systemName: "plus.circle")
+                  .font(.body)
+          }
+        }
+      }
     }
+    .navigationViewStyle(StackNavigationViewStyle())
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- SwiftUI_PropertyWrapper/SwiftUI_PropertyWrapperApp.swift
+++ SwiftUI_PropertyWrapper/SwiftUI_PropertyWrapperApp.swift
@@ -12,6 +12,7 @@ struct SwiftUI_PropertyWrapperApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.textCase, .uppercase)
         }
     }
 }
