2dccac70f6bfc56382fb15d0f347677b41e42610
soduma <soduma@naver.com>
2022-05-03 09:41:43 +0900

    Initial Commit

---
+++ SwiftUI_PropertyWrapper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_PropertyWrapper
+//
+//  Created by 장기화 on 2022/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_PropertyWrapper/SwiftUI_PropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PropertyWrapperApp.swift
+//  SwiftUI_PropertyWrapper
+//
+//  Created by 장기화 on 2022/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
